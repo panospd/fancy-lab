0e75b947a38b2f941a89e26d250bc5265555b962
Panos Anastasiadis <anastapd@gmail.com>
2021-06-06 13:17:46 +0100

    added cart icon and ajaxified cart count updates

--- inc/wc-modifications.php
+++ inc/wc-modifications.php
@@ -55,3 +55,22 @@ function fancy_lab_wc_modify() {
 
 add_action('wp', 'fancy_lab_wc_modify');
 
+/**
+ * Show cart contents / total Ajax
+ */
+add_filter( 'woocommerce_add_to_cart_fragments', 'fancy_lab_woocommerce_header_add_to_cart_fragment' );
+
+function fancy_lab_woocommerce_header_add_to_cart_fragment( $fragments ) {
+	global $woocommerce;
+
+	ob_start();
+
+	?>
+	<span class="items">
+       <?php echo WC()->cart->get_cart_contents_count(); ?>
+    </span>
+	<?php
+	$fragments['span.items'] = ob_get_clean();
+	return $fragments;
+}
+
